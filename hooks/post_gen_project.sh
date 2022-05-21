323c7fd117f2eaceacd7d54a1cef93832b135849
David Wolf <68775926+devidw@users.noreply.github.com>
2022-05-21 10:14:36 +0200

    add conda

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
 git init
+
+conda env create -n {{cookiecutter.conda_env_name}} --file ./env.yml
+
+# conda init $SHELL
+# conda init zsh
+
+# conda activate {{cookiecutter.conda_env_name}}
